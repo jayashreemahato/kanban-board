4b381e23e2046ecccebdef4593ff5125556ae5d8
jayashreemahato <jayashreemahato1811@gmail.com>
2024-12-01 11:27:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React from "react";
-import Header from "./components/Header";
-import KanbanBoard from "./components/KanbanBoard";
+import KanbanBoard from './components/KanbanBoard';
 
 function App() {
   return (
-    <div>
-      <Header />
+    <div className="App">
       <KanbanBoard />
     </div>
   );
