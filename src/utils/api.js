41bac8bee8abab6b9f8330168985aa9d2fa2d8b0
jayashreemahato <jayashreemahato1811@gmail.com>
2024-12-01 11:06:40 +0530

    Create api.js

---
+++ src/utils/api.js
@@ -0,0 +1,5 @@
+export const fetchData = async () => {
+  const response = await fetch('https://api.quicksell.co/v1/internal/frontend-assignment');
+  if (!response.ok) throw new Error('Failed to fetch data');
+  return response.json();
+};
